0cd22035f845a7c73c4d0fda9b057451909449c4
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-02-22 00:42:44 -0700

    lab complete

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [selectedCat, setCat] = useState("All")
+  console.log(selectedCat)
+
+  const newArray = items.filter((item) => {
+    return selectedCat === item.category
+  })
+
+  function doThis (e) {
+    setCat(e.target.value)
+  } 
+
+  function mapItems(array) {
+    return array.map((item) => ( <Item key={item.id} name={item.name} category={item.category} /> ))
+  }
+
+  console.log(newArray)
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={doThis} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +31,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {selectedCat === "All" ? mapItems(items) : mapItems(newArray)}
       </ul>
     </div>
   );
